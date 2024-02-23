edb999c987547770c02872c1092f4c85623e76cf
Dmitry [Honor] <aleshkindima25@gmail.com>
2024-02-23 19:49:00 +0600

    Добавлены header и logo. Создан файлик, в котором собираются все настройки среды
    Оказывается, если 3 дня пилить код, делать коммиты и не пушить, а потом снести винду, будет больно...

--- app/src/main/java/com/example/androidlab1/MainActivity.kt
+++ app/src/main/java/com/example/androidlab1/MainActivity.kt
@@ -4,43 +4,70 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.example.androidlab1.ui.theme.AndroidLab1Theme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
-            AndroidLab1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    AndroidLab1Theme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                DotaScreen()
+            }
+        }
+    }
+
+@Composable
+private fun ApplySystemBarColors() {
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setStatusBarColor(color = Color.Transparent)
+        systemUiController.setNavigationBarColor(color = Color.Transparent)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun DotaScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    DotaScreenHeader()
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun GreetingPreview() {
     AndroidLab1Theme {
-        Greeting("Android")
+        MainScreen()
     }
 }
